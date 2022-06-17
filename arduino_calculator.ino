a3366505fb0400a0f3fed0d49f83dc7e01de7ae3
Murillo Martins <murillomartins@uol.com.br>
2022-06-17 02:14:54 -0300

    trying to make math work

--- arduino_calculator.ino
+++ arduino_calculator.ino
@@ -2,23 +2,50 @@
  
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 bool LastButtonOperator = false;
+int position = 0;
 int blocks = 0;
-int ButtonZero = 22;
-int ButtonOne = 23;
-int ButtonTwo = 24;
-int ButtonPlus = 38; // branco
-int ButtonEqual = 39; // Verde
-int ButtonMinus = 40; // roxo
-int ButtonClear = 53; // laranja
-int num[15];
+const int ButtonZero = 22;
+const int ButtonOne = 23;
+const int ButtonTwo = 24;
+const int ButtonThree = 25;
+const int ButtonFour = 26;
+const int ButtonPlus = 38; // branco
+const int ButtonEqual = 39; // Verde
+const int ButtonMinus = 40; // roxo
+const int ButtonClear = 53; // laranja
+String num[15];
 
-void setup()
-{
+void logical_number(String button_number) {
+  LastButtonOperator = false;
+  num[blocks] = button_number;
+  blocks += 1;
+  position += 1;
+  lcd.print(button_number);
+}
+
+void logical_operator(String button_operator) {
+  if (LastButtonOperator == false) {
+    LastButtonOperator = true;
+    num[blocks] = button_operator;
+    blocks += 1;
+    position += 1;
+    lcd.print(button_operator);
+  }
+  else {
+    num[blocks - 1] = button_operator;
+    lcd.setCursor(position - 1, 0);
+    lcd.print(button_operator);
+  }
+}
+
+void setup() {
   lcd.begin(16, 2);
   Serial.begin(9600);
   pinMode(ButtonZero, INPUT_PULLUP);
   pinMode(ButtonOne, INPUT_PULLUP);
   pinMode(ButtonTwo, INPUT_PULLUP);
+  pinMode(ButtonThree, INPUT_PULLUP);
+  pinMode(ButtonFour, INPUT_PULLUP);
   pinMode(ButtonPlus, INPUT_PULLUP);
   pinMode(ButtonEqual, INPUT_PULLUP);
   pinMode(ButtonMinus, INPUT_PULLUP);
@@ -27,15 +54,12 @@ void setup()
  
 void loop() {
   // Números
-  if (blocks != 0 && LastButtonOperator == false) {
+  if (position != 0 && LastButtonOperator == false) {
     if (digitalRead(ButtonZero) == LOW) {
       delay(50);
       if (digitalRead(ButtonZero) == HIGH) {
         delay(50);
-        LastButtonOperator = false;
-        num[blocks] = 0;
-        blocks = blocks + 1;
-        lcd.print(0);
+        logical_number("0");
       }
     }
   }
@@ -44,10 +68,7 @@ void loop() {
     delay(50);
     if (digitalRead(ButtonOne) == HIGH) {
       delay(50);
-      LastButtonOperator = false;
-      num[blocks] = 1;
-      blocks = blocks + 1;
-      lcd.print(1);
+      logical_number("1");
     }
   }
 
@@ -55,51 +76,67 @@ void loop() {
     delay(50);
     if (digitalRead(ButtonTwo) == HIGH) {
       delay(50);
-      LastButtonOperator = false;
-      num[blocks] = 2;
-      blocks = blocks + 1;
-      lcd.print(2);
+      logical_number("2");
+    }
+  }
+
+  if (digitalRead(ButtonThree) == LOW) {
+    delay(50);
+    if (digitalRead(ButtonThree) == HIGH) {
+      delay(50);
+      logical_number("3");
+    }
+  }
+
+  if (digitalRead(ButtonFour) == LOW) {
+    delay(50);
+    if (digitalRead(ButtonFour) == HIGH) {
+      delay(50);
+      logical_number("4");
     }
   }
 
 
   // Operadores
-  if (blocks != 0) {
-    if (digitalRead(ButtonPlus) == LOW) {
+  if (position != 0) {
+    if (digitalRead(ButtonEqual) == LOW) {
       delay(50);
-      if (digitalRead(ButtonPlus) == HIGH) {
+      if (digitalRead(ButtonEqual) == HIGH) {
         delay(50);
-        if (LastButtonOperator == false) {
-          LastButtonOperator = true;
-          num[blocks] = "+";
-          blocks = blocks + 1;
-          lcd.print("+");
+        for(int i = 0; i < blocks; i++) {
+          Serial.println(num[i]);
         }
-        else {
-          num[blocks - 1] = "+";
-          lcd.setCursor(blocks - 1, 0);
-          lcd.print("+");
-        }
+        /*String sum = num[0][0] + num[1][0];
+        String sum1 = num[0][1] + num[1][1];
+        String sum2 = sum + op[0];
+        int sumx = sum.toInt();
+        int sumy = sum1.toInt();
+        int sumz = sum2.toInt();
+        Serial.println(sum);
+        Serial.println(sum1);
+        Serial.println(op[0]);
+        Serial.println(sum2);
+        Serial.println(sumz + sumy);*/
       }
     }
   }
 
-  if (blocks != 0) {
+  if (position != 0) {
     if (digitalRead(ButtonPlus) == LOW) {
       delay(50);
       if (digitalRead(ButtonPlus) == HIGH) {
         delay(50);
-        if (LastButtonOperator == false) {
-          LastButtonOperator = true;
-          num[blocks] = "-";
-          blocks = blocks + 1;
-          lcd.print("-");
-        }
-        else {
-          num[blocks - 1] = "-";
-          lcd.setCursor(blocks - 1, 0);
-          lcd.print("-");
-        }
+        logical_operator("+");
+      }
+    }
+  }
+
+  if (position != 0) {
+    if (digitalRead(ButtonMinus) == LOW) {
+      delay(50);
+      if (digitalRead(ButtonMinus) == HIGH) {
+        delay(50);
+        logical_operator("-");
       }
     }
   }
@@ -110,10 +147,8 @@ void loop() {
     delay(50);
     if (digitalRead(ButtonClear) == HIGH) {
       delay(50);
-      for(int i = 0; i < blocks; i++) {
-        Serial.println(num[i]);
-      }
-      blocks = 0;
+      String num[15];
+      blocks, position = 0;
       lcd.clear();
     }
   }
